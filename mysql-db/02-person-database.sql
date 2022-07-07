36259887f015f1db91aad4094a2092bde6e1e299
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2022-07-06 22:19:47 -0300

    Database Updates

--- mysql-db/02-person-database.sql
+++ mysql-db/02-person-database.sql
@@ -3,16 +3,7 @@ CREATE TABLE `users` (
   `name` varchar(64) NOT NULL,
   `email` varchar(100) NOT NULL UNIQUE,
   `password` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`) UNIQUE KEY `email` (`email`)
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-
--- Password: Test1234
-INSERT INTO
-  base_api.users (id, name, email, password)
-VALUES
-  (
-    '01G75V6RSB2796MJ1A1RJ0KWG4',
-    'Jhonny Deep',
-    'teste@email.com',
-    '31df522655f791eb3ee96379e020a797672236b7fa298d1413d904e1d762ba2a'
-  );
